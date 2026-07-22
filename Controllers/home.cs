f644b6251d3c94f85f1131ebafc90a7f49ba5cee
a.moghanloo <a.moghanloo@irisaco.com>
2026-07-22 13:35:19 +0330

    ping

--- Controllers/home.cs
+++ Controllers/home.cs
@@ -1,3 +1,6 @@
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.Differencing;
 using testRequestRabbitMQ.BLL;
@@ -7,18 +10,18 @@ namespace testRequestRabbitMQ.Controllers;
 [Route("api/[controller]/[action]")]
 public class homeController : ControllerBase
 {
-   private readonly ILogger<homeController> _logger;
+    private readonly ILogger<homeController> _logger;
     private TestDbContext _TestDbContext;
-    public homeController( ILogger<homeController> logger, TestDbContext testDbContext)
+    public homeController(ILogger<homeController> logger, TestDbContext testDbContext)
     {
-      _logger = logger;
+        _logger = logger;
         _TestDbContext = testDbContext;
     }
     [HttpGet]
     public IActionResult Index()
     {
         _logger.LogInformation("--------Index---------" + new Random().Next(100));
-       var  audit =  HttpContext.Items["DATA_RMG"] as RequestAuditLog ??new RequestAuditLog();
+        var audit = HttpContext.Items["DATA_RMG"] as RequestAuditLog ?? new RequestAuditLog();
         if (audit != null)
         {
             Thread.Sleep(200);
@@ -34,7 +37,7 @@ public class homeController : ControllerBase
             HttpContext.Items["DATA_RMG"] = audit;
 
         }
-        return Ok("Helo word"); 
+        return Ok("Helo word");
     }
 
     [HttpGet]
@@ -44,5 +47,44 @@ public class homeController : ControllerBase
         return Ok(_TestDbContext.RequestAuditLogs.ToList());
     }
 
+    [HttpGet]
+    public async Task<IActionResult> ping(string host)
+    {
+        //var ss = await PingHostAsync(host);//SendAdvancedPingAsync(host);
+         using var ping = new Ping();
+         var reply = await   ping.SendPingAsync(host);
+         
+        if (reply.Status == IPStatus.Success)
+        {
+           return Ok($"آدرس: {reply.Address} زمان پاسخ: {reply.RoundtripTime} میلی‌ثانیهطول بافر: {reply.Buffer.Length}");
+              
+        }
+        else
+        {
+            return BadRequest($"پینگ ناموفق: {reply.Status}");
+        }
+    }
+
+    public async Task<PingReply> SendAdvancedPingAsync(string host)
+    {
+        using var ping = new Ping();
+
+        // تنظیمات (TTL و عدم تکه‌تکه شدن)
+        var options = new PingOptions
+        {
+            Ttl = 64,               // حداکثر تعداد هاپ
+            DontFragment = true
+        };
+
+        // داده ارسالی (مشابه بافر پیش‌فرض ۳۲ بایت)
+        byte[] buffer = Encoding.ASCII.GetBytes("PingData");
+
+        // تایم‌اوت ۳ ثانیه‌ای
+        int timeout = 3000;
+
+        return await ping.SendPingAsync(host, timeout, buffer, options);
+    }
+
      
+
 }
